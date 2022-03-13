77b0e53422e5cb9fffe38a0be01dd68c2de40080
Thurzas <mathieu.miot@free.fr>
2022-03-13 21:55:02 +0100

    added colors for stickers

--- js/Main.js
+++ js/Main.js
@@ -1,5 +1,6 @@
 const rotate=8;
 const x = 16;
+let colors = ["note","PinkNote","GNote","PurpleNote"];
 jQuery.fn.rotate = function(degrees) {
     $(this).css(
       {
@@ -7,10 +8,27 @@ jQuery.fn.rotate = function(degrees) {
       });
     return $(this);
 };
+jQuery.fn.randomize = function() {
+  let c=colors[Math.trunc(Math.random()*4)];
+  let classes = $(this).attr("class").split(" ");
+  let s="";
+  for(var i =0;i<classes.length;i++)
+  {
+    if(classes[i].localeCompare("note")==0)
+      classes[i]=c;    
 
+    s+=classes[i]+" ";
+  }
+  $(this).attr("class",s);
+/*  $(this).css(
+    {
+      'background-color': '#'+c,
+      
+    });*/
+  return $(this);
+};
 $(function() {
   $(".dot").each(function(){
-      console.log($(this).parent());
     $(this).css({
       'background-color': 'rgb('+Math.random()*255+','+Math.random()*255+','+Math.random()*255+')',
       'margin-left': ($(this).parent().width()*0.5+Math.random()*10-5)+'px',
@@ -23,5 +41,6 @@ $(function() {
     let r = Math.random()*2*rotate-rotate;
     $(this).draggable();
     $(this).rotate(r);
+    $(this).randomize();
   });
 });
